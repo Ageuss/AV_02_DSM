fa3f5d2e024ec16089c4ba727c32e8f70a935e2b
Ageu <ageu.costa@fitbank.com.br>
2022-06-01 16:47:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+
+import { SafeAreaView, Text, StyleSheet, View, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
+import api from './api';
+
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [filtered, setFiltered] = useState([]);
+  const [defaultRating, setDefaultRating] = useState(2);
+  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+
+  const filter = (fieldText) => {
+    if (fieldText) {
+      api.get(fieldText).then((Response) => {
+        for (var i = 0; i < Response.data.hits.length; i++) {
+          if (Response.data.hits) {
+            setFiltered(Response.data.hits);
+          }
+        }
+        setSearch(fieldText);
+      });
+    }
+  };
+
+  const RatingBar = () => {
+    return (
+      <View>
+        {maxRating.map((item, key) => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              key={item}
+              onPress={() => setDefaultRating(item)}>
+              <Image
+                style={styles.starImageStyle}
+                source={
+                  item <= defaultRating
+                    ? require('./assets/estrela.png')
+                    : require('./assets/estrela.png')
+                }
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
+  const ViewItems = ({ item }) => {
+    return (
+      <Text style={styles.items}>
+        {'Author: '}
+        {item.author + '\n'}
+        {'Title: '}
+        {item.title + '\n'}
+        {'Url: '}
+        {item.url + '\n'}
+        {'Rate: '}
+        {item.rate}
+        <RatingBar />
+        <Text>
+          {defaultRating} / {Math.max.apply(null, maxRating)}
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => alert(defaultRating)}>
+        </TouchableOpacity>
+
+      </Text>
+    );
+  };
+
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>Books</Text>
+        <TextInput
+          style={styles.inpt}
+          onChangeText={(fieldText) => setSearch(fieldText)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Buscar"
+        />
+        <Button
+          onPress={() => filter(search)}
+          title="Buscar"
+          color="green"
+        />
+        <FlatList
+          data={filtered}
+          renderItem={ViewItems}
+        />
+      </View>
+    </SafeAreaView>
+
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    backgroundColor: 'white',
+  },
+  text: {
+    textAlign: "center",
+    fontSize: '150%',
+    marginBottom: '4%'
+  },
+  items: {
+    backgroundColor: '#0066CC',
+    padding: 8,
+    marginTop: 8,
+    color: 'white',
+  },
+  inpt: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+  },
+
+});
+
+export default App;
---
+++ Rating.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { H1 } from 'native-base';
+
+// export default function Rating(props) {
+//     return (
+//         <H1>Rating: {props.Rating}</H1>
+//     );
+// }
